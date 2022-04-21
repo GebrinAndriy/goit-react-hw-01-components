b91a5ef7f422a93c2500bfae0cdbda05f641fddb
GebrinAndriy <gebrinandriy@gmail.com>
2022-04-21 18:08:25 +0200

    101%

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,17 +6,9 @@ import { FriendsList } from "./task3/friends.jsx";
 import friends from "./task3/friends.json"
 import { TransactionHistory } from "./task4/transactions.jsx";
 import transactions from "./task4/transactions.json"
-const setBg = () => {
-  const items = document.querySelectorAll(".stat-item");
-  items.forEach(item => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    item.style.backgroundColor = "#" + randomColor;
-  })
-} 
 export const App = () => {
   const {username, tag, location, avatar, stats} = user;
   const title = "Upload stats";
-  setBg(); 
   return (
     <div>
       <Profile
